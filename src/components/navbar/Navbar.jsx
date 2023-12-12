579e430ee7630ca2c28f186dfddc2c02dd152f8b
jibon-roy <mailme.jibon@gmail.com>
2023-12-12 16:53:34 +0600

    some changes in style

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -23,17 +23,17 @@ const Navbar = () => {
                 <div className="drawer-content flex flex-col">
                     {/* Navbar */}
                     <div className="w-full navbar">
-                        <div className="flex-none md:hidden">
+                        {/* <div className="flex-none md:hidden">
                             <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                             </label>
-                        </div>
+                        </div> */}
                         <div className="flex-1 px-2 text-5xl mx-2 font-semibold">
                             <span className="px-4 py-2 rounded-l-xl" style={{ backgroundColor: color, border: `2px solid ${color}`, color: 'black' }}>Dev </span>
                             <span style={{ border: `2px solid ${color}`, color: whiteColor }} className="px-4 py-2 rounded-r-xl">Jibon</span>
                         </div>
 
-                        <div className="flex-none hidden md:block">
+                        <div className="flex-none">
                             <ul className="menu menu-horizontal flex items-center justify-center gap-3">
                                 {navMenu}
                             </ul>
@@ -41,13 +41,13 @@ const Navbar = () => {
                     </div>
 
                 </div>
-                <div className="drawer-side">
+                {/* <div className="drawer-side">
                     <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-80 min-h-full z-50" style={{ background: color + '99' }}>
                         {navMenu}
                     </ul>
 
-                </div>
+                </div> */}
             </div>
         </div>
     );
--- src/pages/home/hero/Hero.jsx
+++ src/pages/home/hero/Hero.jsx
@@ -19,21 +19,23 @@ const Hero = ({ color, whiteColor }) => {
 
     return (
         <>
-            <div className="hero min-h-full md:min-h-[60vh] mb-20 z-0" style={{ color: whiteColor }}>
-                <div className="hero-content md:gap-20 justify-center max-w-5xl flex-col lg:flex-row">
-                    <img src="./jibon.png" style={{ backgroundColor: color }} className="max-w-sm rounded-lg shadow-2xl" />
-                    <div>
-                        <h1 className="text-5xl font-bold">
-                            <TextTransition springConfig={presets.wobbly}>{TEXTS[index % TEXTS.length]}</TextTransition>
-                        </h1>
-                        <p className="py-10" style={{ color: color }} >
-                            Hello I&#39;m Jibon Krishna Roy an Enthusiastic MERN stack developer dedicated to crafting seamless web experiences. Proficient in MongoDB, Express.js, React.js, and Node.js, I specialize in building robust, scalable websites. Committed to clean code and innovative problem-solving, I thrive in collaborative environments. Let&#39;s elevate your digital presence together through cutting-edge web development.
-                        </p>
-                        <a href={MyResume} download={'resume.pdf'} className="btn text-black" style={{ backgroundColor: color }} >Download Resume</a>
+            <div className='mb-40'>
+                <div className="hero min-h-full md:min-h-[60vh] mb-20 z-0" style={{ color: whiteColor }}>
+                    <div className="hero-content md:gap-20 justify-center max-w-5xl flex-col lg:flex-row">
+                        <img src="./jibon.png" style={{ backgroundColor: color }} className="max-w-sm rounded-lg shadow-2xl" />
+                        <div>
+                            <h1 className="text-5xl font-bold">
+                                <TextTransition springConfig={presets.wobbly}>{TEXTS[index % TEXTS.length]}</TextTransition>
+                            </h1>
+                            <p className="py-10" style={{ color: color }} >
+                                Hello I&#39;m Jibon Krishna Roy an Enthusiastic MERN stack developer dedicated to crafting seamless web experiences. Proficient in MongoDB, Express.js, React.js, and Node.js, I specialize in building robust, scalable websites. Committed to clean code and innovative problem-solving, I thrive in collaborative environments. Let&#39;s elevate your digital presence together through cutting-edge web development.
+                            </p>
+                            <a href={MyResume} download={'resume.pdf'} className="btn text-black" style={{ backgroundColor: color }} >Download Resume</a>
+                        </div>
                     </div>
                 </div>
+                <Line></Line>
             </div>
-            <Line></Line>
         </>
     );
 };

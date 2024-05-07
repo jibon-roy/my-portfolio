5d71d21733f92c046ecc040992aa521eadf79efa
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-05-07 21:11:10 +0600

    some update in Hero and added new resume

--- src/components/line/Line.jsx
+++ src/components/line/Line.jsx
@@ -3,7 +3,7 @@ import useColor from '../../hooks/useColor';
 const Line = () => {
     const { color } = useColor()
     return (
-        <div className='w-[100vw] absolute left-0 h-1 flex -rotate-3' style={{ backgroundColor: color }}></div>
+        <div className='w-[100%] absolute left-0 h-1 flex -rotate-3' style={{ backgroundColor: color }}></div>
     );
 };
 
